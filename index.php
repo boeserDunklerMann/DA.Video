1af62397e7ece18bf0da363362b8d6292e5650dc
André <andre.dunkel@t-online.de>
2025-01-14 13:45:13 +0100

    table stuff removed

--- index.php
+++ index.php
@@ -2,25 +2,13 @@
 <head>
 </head>
 <body>
-<table border="0">
 <?php
-	$i=0;
 	foreach (glob("*.mp4") as $fname)
 	{
-		if ((($i % 3)==0) || $i==0)
-		{
 ?>
-	<tr>
-<?php
-		}
-		$i++;
-?>
-		<td>
 <a href="play.php?fname=<?php print($fname); ?>">
 <img src="preview/<?php print($fname); ?>.gif" />
 </a>
-		</td>
-
 <?php
 	}
 ?>
