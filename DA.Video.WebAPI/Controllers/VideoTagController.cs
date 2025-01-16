c50edf7f750b34bea2c9709535748099a3222177
André <andre.dunkel@t-online.de>
2025-01-16 11:22:01 +0100

    GetAllVideoTagsAsync created in VideoTagController

---
+++ DA.Video.WebAPI/Controllers/VideoTagController.cs
@@ -0,0 +1,25 @@
+﻿using DA.Video.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DA.Video.WebAPI.Controllers
+{
+	/// <ChangeLog>
+	/// <Create Datum="16.01.2025" Entwickler="DA" />
+	/// </ChangeLog>
+	[Route("api/[controller]")]
+	[ApiController]
+	public class VideoTagController : ControllerBase
+	{
+		public VideoTagController(IConfiguration cfg, ILogger<VideoController> log, IDbContext db) : base(cfg, log, db)
+		{
+		}
+
+		[HttpGet]
+		public async Task<IEnumerable<VideoTag>> GetAllVideoTagsAsync()
+		{
+			return await context.Tags.ToListAsync();
+		}
+	}
+}
